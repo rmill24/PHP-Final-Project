d64e422020dcd43c5b271bc65a6f8ec8e0b90f67
rmill24 <202311529@fit.edu.ph>
2025-07-15 23:05:03 +0800

    feat: add dynamic page titles

--- index.php
+++ index.php
@@ -8,6 +8,17 @@ if (!in_array($page, $allowed_pages)) {
     $page = 'home';
 }
 
+// Set title based on page
+$page_titles = [
+    'home' => 'Home | Venusia',
+    'about' => 'About Us | Venusia',
+    'store' => 'Store | Venusia',
+    'cart' => 'Your Cart | Venusia',
+    'payment' => 'Payment | Venusia'
+];
+$page_title = $page_titles[$page] ?? 'Venusia';
+
+// Get page content
 ob_start();
 include "pages/$page.php";
 $page_content = ob_get_clean();

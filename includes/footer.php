06741559e3b080332c79ee74a0ccc2628ef0b37d
rmill24 <202311529@fit.edu.ph>
2025-07-16 19:45:47 +0800

    feat: add boostrap script

--- includes/footer.php
+++ includes/footer.php
@@ -58,6 +58,7 @@
   <!-- Page-specific JS -->
   <?php if (file_exists("assets/js/{$current_page}.js")): ?>
     <script src="assets/js/<?= $current_page ?>.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
   <?php endif; ?>
 </body>
 </html>

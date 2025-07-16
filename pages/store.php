81d5a4fbc6ff83bcc7c6d3de6647cb39b2f7a24a
rmill24 <202311529@fit.edu.ph>
2025-07-16 19:25:58 +0800

    feat: add store page

---
+++ pages/store.php
@@ -0,0 +1,62 @@
+<div class="container">
+
+    <div class="store-nav">
+        <h1>Venusia Collection</h1>
+        <nav>
+            <a href="#">All Items</a>
+            <a href="#">Tops</a>
+            <a href="#">Bottoms</a>
+            <a href="#">Dresses</a>
+            <a href="#">Outerwear</a>
+        </nav>
+    </div>
+
+    <div class="product-grid">
+
+        <div class="product-card">
+            <img src="https://plus.unsplash.com/premium_photo-1675107359683-6d9b90aa593b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            <div class="product-info">
+                <h3>Silky white blouse</h3>
+                <div class="price-info">
+                    <p>P350</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="product-card">
+            <img src="https://images.unsplash.com/photo-1635073243626-36ee25e0f77e?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            <div class="product-info">
+                <h3>Silky white blouse</h3>
+                <div class="price-info">
+                    <p>P350</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="product-card">
+            <img src="https://images.unsplash.com/photo-1635073243626-36ee25e0f77e?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            <div class="product-info">
+                <h3>Silky white blouse</h3>
+                <div class="price-info">
+                    <p>P350</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="product-card">
+            <img src="https://images.unsplash.com/photo-1635073243626-36ee25e0f77e?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            <div class="product-info">
+                <h3>Silky white blouse</h3>
+                <div class="price-info">
+                    <p>P350</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>

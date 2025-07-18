2a3db0670a3ce86c724bee62aeb591592dd17a10
rmill24 <202311529@fit.edu.ph>
2025-07-19 04:53:46 +0800

    feat: add icon in browser tab

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($page_title ?? 'Venusia') ?></title>
+    <link rel="icon" href="assets/img/venusia_logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/global.css">
